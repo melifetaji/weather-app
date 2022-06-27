dc5855fd3adc17f5eb34a49af567f78cca3e23f5
Meli <melifetaji@Melis-MacBook-Pro.local>
2022-06-27 17:19:42 +0200

    Added Running console.log

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,8 @@ const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
 
+console.log("Running...");
+
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
